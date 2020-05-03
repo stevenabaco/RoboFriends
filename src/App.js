2b0ac013e14c3b93fe337748c096561960185070
Steven Abaco <steve@wizardrywd.com>
2020-05-03 01:11:27 -0500

    Fix bug causing an error

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        CardList robots={filteredRobots} />
+                        <CardList robots={filteredRobots} />
                     </Scroll>
                     
                 </div>
